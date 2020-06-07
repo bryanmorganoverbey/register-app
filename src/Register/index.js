b8941424c680717cb9178d81bc2999dd5621edc2
Bryan Overbey <bryanmorganoverbey@gmail.com>
2020-06-06 19:10:53 -0500

    add validation and error handling

--- src/Register/index.js
+++ src/Register/index.js
@@ -1,22 +1,15 @@
 import React from 'react'
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import {
-    Box,
-    Container,
     makeStyles,
     Grid,
-    CircularProgress,
     Button,
     TextField,
     Card,
     CardContent,
     CardHeader,
-    Typography,
-    Select,
-    InputLabel,
     MenuItem,
-    FormControl
 } from '@material-ui/core'
 import Background from '../images/background.jpeg'
 var UsaStates = require('usa-states').UsaStates;
@@ -32,7 +25,6 @@ const useStyles = makeStyles(() => ({
     },
     registerCard: {
         width: '30%',
-        
     },
     outterGrid: {
         display: 'flex',
@@ -50,16 +42,27 @@ const RegisterSchema = Yup.object().shape({
         .min(2, 'Too Short!')
         .max(50, 'Too Long!')
         .required('Required'),
+    npiNumber: Yup.string()
+        .min(6, 'Too Short!')
+        .max(10, 'Too Long!')
+        .required('Required'),
+    streetAddress: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    state: Yup.string()
+        .required('Required'),
+    zipCode: Yup.string()
+        .matches(/(^\d{5}$)|(^\d{5}-\d{4}$)/, 'Must be a valid zip code')
+        .required('Required'),
+    phoneNumber: Yup.string()
+        .matches(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/, 'Must be a valid phone number')
+        .required('Required'),
     email: Yup.string()
         .email('Invalid email')
         .required('Required'),
 });
 const usStates = new UsaStates();
-const stateSelectNodes = usStates?.states.map(state => {
-    return (
-        <MenuItem value={state.abreviation}>{state.name}</MenuItem>
-    )
-})
 
 const Register = () => {
     const classes = useStyles();
@@ -71,14 +74,16 @@ const Register = () => {
                     firstName: '',
                     lastName: '',
                     npiNumber: '',
-                    businessAddress: '',
+                    streetAddress: '',
+                    state: '',
+                    zipCode: '',
                     phoneNumber: '',
                     email: '',
                 }}
                 validationSchema={RegisterSchema}
                 onSubmit={values => {
                     // same shape as initial values
-                    alert(values);
+                    alert(JSON.stringify(values));
                 }}
             >
                 {({ values,
@@ -101,6 +106,7 @@ const Register = () => {
                                 <Form>
                                     <Grid>
                                         <TextField
+                                            error={errors.firstName && touched.firstName}
                                             label="First Name"
                                             name="firstName"
                                             value={values.firstName}
@@ -113,6 +119,7 @@ const Register = () => {
                                         </Grid>
                                     <Grid>
                                         <TextField
+                                            error={errors.lastName && touched.lastName}
                                             label="Last Name"
                                             name="lastName"
                                             value={values.lastName}
@@ -125,6 +132,7 @@ const Register = () => {
                                     </Grid>
                                     <Grid>
                                         <TextField
+                                            error={errors.npiNumber && touched.npiNumber}
                                             label="NPI Number"
                                             name="npiNumber"
                                             value={values.npiNumber}
@@ -134,54 +142,70 @@ const Register = () => {
                                             margin="normal"
                                             fullWidth
                                         />
-                                        </Grid>
-                                        <Grid>
-                                            <TextField
-                                                label="Street Address"
-                                                name="streetAddress"
-                                                value={values.streetAddress}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                helperText={(errors.streetAddress&& touched.streetAddress) && errors.streetAddress}
-                                                margin="normal"
-                                                fullWidth
+                                    </Grid>
+                                    <Grid>
+                                        <TextField
+                                            error={errors.streetAddress && touched.streetAddress}
+                                            label="Street Address"
+                                            name="streetAddress"
+                                            value={values.streetAddress}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            helperText={(errors.streetAddress && touched.streetAddress) && errors.streetAddress}
+                                            margin="normal"
+                                            fullWidth
                                             />
-                                        </Grid>
-                                        <Grid>
-                                            <FormControl fullWidth>
-                                                <InputLabel id="select-label">State</InputLabel>
-                                                <Select
-                                                    label='select-label'
-                                                    name="state"
-                                                    value={values.state}
-                                                    onChange={handleChange}
-                                                    onBlur={event => {
-                                                        event.target.name = 'state';
-                                                        handleBlur(event);
-                                                    }}
-                                                    helperText={(errors.state && touched.state) && errors.this.state}
-                                                    margin="normal"
-                                                    >
-                                                    <MenuItem value="">
-                                                    <em>None</em>
-                                                    </MenuItem>
-                                                    {stateSelectNodes}
-                                                </Select>
-                                            </FormControl>
-                                        </Grid>
-                                        <Grid>
+                                    </Grid>
+                                        <Grid >
                                             <TextField
-                                                error={errors.zipCode && touched.zipCode}
-                                                label="Zip Code"
-                                                name="zipCode"
-                                                value={values.zipCode}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                helperText={(errors.zipCode && touched.zipCode) && errors.zipCode}
-                                                margin="normal"
+                                            error={errors.state && touched.state}
+                                            helperText={(errors.state && touched.state) && errors.state}
+                                            id="state"
+                                            select
+                                            label="State"
+                                            className={classes.textField}
+                                            value={values.state}
+                                            onChange={handleChange("state")}
+                                            margin="normal"
                                                 fullWidth
-                                            />
-                                        </Grid>
+                                                style={{ textAlign: 'left'}}
+                                            >
+                                            <MenuItem value="">
+                                                <em>None</em>
+                                            </MenuItem>
+                                            {usStates?.states.map(state => (
+                                                <MenuItem key={state.abbreviation} value={state.abbreviation}>
+                                                    {state.name}
+                                                </MenuItem>
+                                            ))}
+                                        </TextField>
+                                    </Grid> 
+                                    <Grid>
+                                        <TextField
+                                            error={errors.zipCode && touched.zipCode}
+                                            label="Zip Code"
+                                            name="zipCode"
+                                            value={values.zipCode}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            helperText={(errors.zipCode && touched.zipCode) && errors.zipCode}
+                                            margin="normal"
+                                            fullWidth
+                                        />
+                                    </Grid>
+                                    <Grid>
+                                        <TextField
+                                            error={errors.phoneNumber && touched.phoneNumber}
+                                            label="Phone Number"
+                                            name="phoneNumber"
+                                            value={values.phoneNumber}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            helperText={(errors.phoneNumber && touched.phoneNumber) && errors.phoneNumber}
+                                            margin="normal"
+                                            fullWidth
+                                        />
+                                    </Grid>
                                     <Grid>
                                         <TextField
                                             error={errors.email && touched.email}
